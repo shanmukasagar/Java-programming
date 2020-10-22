11d2dd34cae25766124e4cf0ce079f2f5483f921
shanmukasagar <70527028+shanmukasagar@users.noreply.github.com>
2020-10-22 22:11:59 +0530

    Update and rename MergeSort.java to Experiment_6.java

--- Experiment_6.java
+++ Experiment_6.java
@@ -1,7 +1,7 @@
 import java.io.*;
 import java.util.Scanner;
 
-public class MergeSort{
+public class Experiment_6{
 
 	public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
